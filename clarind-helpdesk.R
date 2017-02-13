167a83c170c35e24d317734f226544829b53119a
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2017-02-13 18:09:26 +0100

    fix points

--- clarind-helpdesk.R
+++ clarind-helpdesk.R
@@ -13,9 +13,11 @@ plot(cumsum(closedsucc), main="CLARIN+D Helpdesk", ylab="Tickets",
      type = "l", pch="-", col = "green", ylim = c(1, ymax))
 points(cumsum(closedunsucc), type = "l", pch = "-", col = "red")
 points(cumsum(merged), type = "l", pch = "-", col = "light green")
-points(newnew, type="b", col = "blue", pch="o")
-points(open, type="b", col = "purple", pch="x")
-legend(1, ymax, c("ΣSuccessful", "ΣUnsuccessful", "Merged", "New", "Open"),
+points(seq(1,16), newnew, type="p", col = "blue", pch="o")
+points(seq(1,16), open, type="p", col = "purple", pch="x")
+legend(1, ymax, c("Successful (cumulative)", "Unsuccessful (cumulative)",
+                  "Merged (cumulative part of)",
+                  "New (at end of month)", "Open (at end of month)"),
                  col = c("green", "red", "light green", "blue", "purple"),
                  pch = c("-", "-", "-", "o", "x"))
 
