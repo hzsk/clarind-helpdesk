d9e837ae57d4278c89277620130b34a4d8ffe045
Tommi A Pirinen <tommi.antero.pirinen@uni-hamburg.de>
2018-09-10 15:32:40 +0200

    some cleaning up spam headers

--- stats/otrs-csv2tsv.bash
+++ stats/otrs-csv2tsv.bash
@@ -3,6 +3,6 @@ E=0
 if test $# -ge 1 ; then
     E=$1
 fi
-sed -e 's/","/	/g' -e 's/^"//' -e 's/"$//' -e 's/viikko//g' -e 's/#//g' \
+sed -e 's/";"/	/g' -e 's/^"//' -e 's/"$//' -e 's/viikko//g' -e 's/#//g' \
     -e "s/	0	/	$E	/g" -e "s/	0	/	$E	/g" -e "s/	0$/	$E/" \
-    -e 's/	$//'
+    -e 's/	$//' | tr -d '"'"'"'\\'
